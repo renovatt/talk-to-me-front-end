8630e22e9c8158f7970b0b5fc35e0f01a2d112eb
will <wlymes@gmail.com>
2024-01-30 09:42:39 -0300

    feat: font and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={rubik.className}>{children}</body>
+      <body className={`${rubik.className} bg-800 text-white`}>{children}</body>
     </html>
   );
 }
