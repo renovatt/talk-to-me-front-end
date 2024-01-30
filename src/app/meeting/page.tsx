74288c4e3485b4c60ee562997ed641793fd0d473
will <wlymes@gmail.com>
2024-01-30 11:19:07 -0300

    feat: meeting and home page

--- src/app/meeting/page.tsx
+++ src/app/meeting/page.tsx
@@ -4,22 +4,21 @@ import Header from "@/components/Header";
 import { Input } from "@/components/Input";
 import Link from "next/link";
 
-export default function Home() {
+export default function Meeting() {
   return (
     <main className="min-h-screen flex flex-col">
       <Header />
       <section className="flex flex-col items-center mx-auto flex-1 justify-center">
         <Container>
-          <article className="bg-600 max-w-[587px] w-full rounded-md overflow-hidden flex-col h-60 flex justify-between items-center">
+          <article className="bg-600 max-w-[587px] w-full rounded-md overflow-hidden h-60 flex-col flex justify-start items-center">
             <section className="w-full flex justify-between items-center">
-              <Link href="/" className="text-700 text-base bg-600 w-1/2 text-center p-2 cursor-pointer">Ingresso</Link>
-              <Link href="/meeting" className="text-white text-base w-1/2 text-center p-2 bg-800 cursor-pointer">Nova reunião</Link>
+              <Link href="/" className="text-white text-base w-1/2 text-center p-2 bg-800 cursor-pointer">Ingresso</Link>
+              <Link href="/meeting" className="text-700 text-base bg-600 w-1/2 text-center p-2 cursor-pointer">Nova reunião</Link>
             </section>
 
-            <form className="space-y-4 p-4">
+            <form className="space-y-4 p-4 mt-10">
               <Input type="text" placeholder="Seu nome" />
-              <Input type="text" placeholder="ID da reunião" />
-              <Button title="Entrar" />
+              <Button title="Criar" />
             </form>
           </article>
         </Container>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import Container from "./Container";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="bg-1000 p-4 border-b border-700">
       <Container>
         <div className="flex justify-between">
+          <Link href="/">
           <Image src="/logo.svg" alt="logo" width={120} height={120} />
-          <Image src="/logo-2.svg" alt="logo" width={40} height={40} />
-        </div>
-      </Container>
-    </header>
+        </Link>
+        <Image src="/logo-2.svg" alt="logo" width={40} height={40} />
+      </div>
+    </Container>
+    </header >
   )
 }
