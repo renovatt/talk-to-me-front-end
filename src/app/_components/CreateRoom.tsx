235ce651820867376627487b72bb3d209b3e307a
will <wlymes@gmail.com>
2024-02-01 14:49:10 -0300

    feat: join and create room redirect

--- src/app/_components/CreateRoom.tsx
+++ src/app/_components/CreateRoom.tsx
@@ -1,15 +1,31 @@
 'use client'
 import Button from '@/components/Button'
 import { Input } from '@/components/Input'
-import { useRef } from 'react'
+import { FormEvent, useRef } from 'react'
 
 export default function CreateRoom() {
   const name = useRef<HTMLInputElement>(null)
 
+  const handleCreateRoom = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (name.current && name.current.value !== '') {
+      sessionStorage.setItem('@chat-username', name.current.value)
+      const roomId = generateRandomString()
+      window.location.href = `/room/${roomId}`
+    }
+  }
+
+  function generateRandomString() {
+    const randomString = Math.random().toString(36).substring(2, 7)
+    return randomString
+  }
+
   return (
     <>
-      <Input placeholder="Seu nome" type="text" ref={name} />
-      <Button title="Criar" type="button" />
+      <form onSubmit={(e) => handleCreateRoom(e)} className="space-y-8">
+        <Input placeholder="Seu nome" type="text" ref={name} />
+        <Button title="Criar" type="submit" />
+      </form>
     </>
   )
 }
--- src/app/_components/JoinRoom.tsx
+++ src/app/_components/JoinRoom.tsx
@@ -1,15 +1,32 @@
+'use client'
 import Button from '@/components/Button'
 import { Input } from '@/components/Input'
-import { useRef } from 'react'
+import { FormEvent, useRef } from 'react'
 
 export default function JoinRoom() {
   const name = useRef<HTMLInputElement>(null)
   const id = useRef<HTMLInputElement>(null)
+
+  const handleJoinRoom = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (
+      name.current &&
+      name.current.value !== '' &&
+      id.current &&
+      id.current.value !== ''
+    ) {
+      sessionStorage.setItem('@chat-username', name.current.value)
+      const roomId = id.current.value
+      window.location.href = `/room/${roomId}`
+    }
+  }
   return (
     <>
-      <Input placeholder="Seu nome" type="text" ref={name} />
-      <Input placeholder="ID da reunião" type="text" ref={id} />
-      <Button title="Entrar" type="button" />
+      <form onSubmit={(e) => handleJoinRoom(e)} className="space-y-8">
+        <Input placeholder="Seu nome" type="text" ref={name} />
+        <Input placeholder="ID da reunião" type="text" ref={id} />
+        <Button title="Entrar" type="submit" />
+      </form>
     </>
   )
 }
