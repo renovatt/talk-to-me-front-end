b03755feb2eaf96e358b8b8510f6776e18f57d29
will <wlymes@gmail.com>
2024-02-02 11:22:18 -0300

    fixed styles

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -7,11 +7,11 @@ interface IMessage {
 export default function ChatMessage({ message, username, time }: IMessage) {
   return (
     <article className="relative mb-2 flex w-full flex-col space-y-2 rounded-lg bg-950 p-2">
-      <h4 className="text-700">{username}</h4>
-      <span className="flex w-full max-w-80 flex-wrap items-center justify-start text-xs">
+      <h4 className="capitalize text-700">{username}</h4>
+      <span className="flex w-full max-w-80 flex-wrap items-center justify-start break-all text-xs">
         {message}
       </span>
-      <span className="flex w-full items-center justify-end text-xs">
+      <span className="flex w-full items-center justify-end text-[9px]">
         {time}
       </span>
     </article>
--- src/components/VideoCam.tsx
+++ src/components/VideoCam.tsx
@@ -5,14 +5,15 @@ export default function VideoCam({
 }: {
   localStream: RefObject<HTMLVideoElement>
 }) {
+  const username = sessionStorage.getItem('@chat-username') as string
   return (
-    <section className="relative flex h-40 w-full max-w-80 items-center justify-center overflow-hidden rounded bg-950 md:h-60 md:w-80">
+    <section className="relative flex h-40 w-full max-w-80 items-center justify-center overflow-hidden rounded bg-950 p-2 md:h-60 md:w-80">
       {localStream.current ? (
         <video autoPlay playsInline ref={localStream} className="rounded" />
       ) : (
         <div className="h-32 w-32 rounded-full bg-800 opacity-70 md:h-40 md:w-40" />
       )}
-      <span className="absolute bottom-2 left-2">Will</span>
+      <span className="absolute bottom-2 left-4 capitalize">{username}</span>
     </section>
   )
 }
